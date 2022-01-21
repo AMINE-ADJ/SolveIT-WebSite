1aa57dcdd5f73cc465b9ae96fb21b46d00f03258
Amine <ka_adjou@esi.dz>
2022-01-21 01:40:39 +0100

    55%

---
+++ animation.js
@@ -0,0 +1,7 @@
+function changeCard(i) {
+    document.querySelector('.speaker'+i).style.width = '20px';
+    document.querySelector('speaker'+i).style.height = '100px';
+    // document.querySelector('speaker'+i).style.Image = 300;
+    
+    
+}
