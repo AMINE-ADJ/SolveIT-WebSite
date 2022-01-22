b129ed774c1693f9d8611fb70b2c664de3e9c5a5
Amine <ka_adjou@esi.dz>
2022-01-22 13:29:29 +0100

    speakers animation

--- animation.js
+++ animation.js
@@ -1,7 +1,23 @@
 function changeCard(i) {
-    document.querySelector('.speaker'+i).style.width = '20px';
-    document.querySelector('speaker'+i).style.height = '100px';
-    // document.querySelector('speaker'+i).style.Image = 300;
-    
-    
+        // const description = ["ana nassim", "ana amine", "ana rania"]; //to add a new p...
+    if( document.getElementById("button"+i).classList.contains('minusButton'+i) ) {
+    //    alert("it's minus dans"+i); 
+       document.getElementsByClassName("speaker"+i)[0].className = "speakerafterminusclick"+i;
+       document.getElementsByClassName("speakerName"+i)[0].className = "speakerNameAfterMinus"+i;
+       document.getElementsByClassName("profession"+i)[0].className = "professionAfterMinus"+i;
+       document.getElementsByClassName("speakerDesciption"+i)[0].className = "speakerDesciptionAfterMinus"+i;
+    //    document.getElementsByClassName("content1")[0].className = "contentdown1";
+       document.getElementById("description"+i).outerHTML = "";
+       document.getElementsByClassName("speakerphotodiv"+i)[0].className = "speakerphotodivafterminusclick"+i;
+       document.getElementsByClassName("minusButton"+i)[0].className = "plusButton"+i;
+     }
+     else if( document.getElementById("button"+i).classList.contains('plusButton'+i) ) { 
+        document.getElementsByClassName("speakerafterminusclick"+i)[0].className = "speakerafterplusclick"+i;
+        document.getElementsByClassName("speakerNameAfterMinus"+i)[0].className = "speakerNameAfterPlus"+i;//++
+        document.getElementsByClassName("professionAfterMinus"+i)[0].className = "professionAfterPlus"+i; //++
+        // document.getElementsByClassName("speakerDesciptionAfterMinus"+i)[0].className = "speakerDesciptionAfterPlus"+i;//++
+        document.getElementsByClassName("speakerphotodivafterminusclick"+i)[0].className = "speakerphotodivafterplusclick"+i;
+        document.getElementsByClassName("plusButton"+i)[0].className = "minusButton"+i;
+     }
+     document.body.addEventListener("click", myFunction,{once:false}); 
 }
