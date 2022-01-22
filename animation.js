b5bf02099b7c0f1d0a4d04b969d9afd1fa98df4d
Amine <ka_adjou@esi.dz>
2022-01-22 18:08:05 +0100

    Speakers Animation done

--- animation.js
+++ animation.js
@@ -1,7 +1,18 @@
 function changeCard(i) {
-  
-        const description = ["Im Nassim 2CP ESI STUDENT, ESMS Mentor", "Im Amine 2CP ESI STUDENT, ESMS Mentee", "Im Rania 1CS STUDENT, ESMS Mentor"]; //to add a new p...
-    if( document.getElementById("button"+i).classList.contains('minusButton'+i) ) {
+   // document.body.addEventListener("click", changeCard ,{once:false}); 
+        const description = ["Description-Nassim", "Description-Amine", "Description-Rania"]; //to add a new p...
+        if( document.getElementById("button"+i).classList.contains('plusButton'+i) ) { 
+         document.getElementsByClassName("speakerafterminusclick"+i)[0].className = "speaker"+i;
+         document.getElementsByClassName("speakerNameAfterMinus"+i)[0].className = "speakerName"+i;//++
+         document.getElementsByClassName("professionAfterMinus"+i)[0].className = "profession"+i; //++
+         document.getElementsByClassName("speakerDesciptionAfterMinus"+i)[0].className = "speakerDesciption"+i;//++
+         document.getElementById("description"+i).innerHTML = description[i];
+       // let node = document.createTextNode ('Hello, I am Arun');
+       // document.getElementById("description"+i).appendChild(node); 
+ 
+         document.getElementsByClassName("speakerphotodivafterminusclick"+i)[0].className = "speakerphotodiv"+i;
+         document.getElementsByClassName("plusButton"+i)[0].className = "minusButton"+i;
+      } else if( document.getElementById("button"+i).classList.contains('minusButton'+i) ) {
     //    alert("it's minus dans"+i); 
        document.getElementsByClassName("speaker"+i)[0].className = "speakerafterminusclick"+i;
        document.getElementsByClassName("speakerName"+i)[0].className = "speakerNameAfterMinus"+i;
@@ -12,17 +23,6 @@ function changeCard(i) {
        document.getElementsByClassName("speakerphotodiv"+i)[0].className = "speakerphotodivafterminusclick"+i;
        document.getElementsByClassName("minusButton"+i)[0].className = "plusButton"+i;
      }
-     else if( document.getElementById("button"+i).classList.contains('plusButton'+i) ) { 
-        document.getElementsByClassName("speakerafterminusclick"+i)[0].className = "speakerafterplusclick"+i;
-        document.getElementsByClassName("speakerNameAfterMinus"+i)[0].className = "speakerNameAfterPlus"+i;//++
-        document.getElementsByClassName("professionAfterMinus"+i)[0].className = "professionAfterPlus"+i; //++
-        document.getElementsByClassName("speakerDesciptionAfterMinus"+i)[0].className = "speakerDesciptionAfterPlus"+i;//++
-        document.getElementById("description"+i).innerHTML = description[i];
-      // let node = document.createTextNode ('Hello, I am Arun');
-      // document.getElementById("description"+i).appendChild(node); 
-
-        document.getElementsByClassName("speakerphotodivafterminusclick"+i)[0].className = "speakerphotodivafterplusclick"+i;
-        document.getElementsByClassName("plusButton"+i)[0].className = "minusButton"+i;
-     }
-     document.body.addEventListener("click", myFunction,{once:false}); 
+    
+     
 }
